0ce9b9aced4f81f3b7b82eb08651c3a5e719b98a
Palakgupta0908 <palakgupta9933@gmail.com>
2021-10-05 16:53:22 +0530

    commit changes

---
+++ Palak Gupta/questions.js
@@ -0,0 +1,92 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented JavaScript?",
+    answer: "Brendan Eich",
+    options: [
+      "Douglas Crockford",
+      "Brendan Eich",
+      "Sheryl Sandberg",
+      "James Gosling"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "VPN stands for?",
+    answer: "Virtual Private Network",
+    options: [
+      "Virtual Private Network",
+      "Virtual Path Network",
+      "Virtual Planning Network",
+      "Virtual Public Network"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not a valid SQL type?",
+    answer: "Decimal",
+    options: [
+      "Float",
+      "Numeric",
+      "Decimal",
+      "Char"
+    ]
+  },
+  {
+  numb: 6,
+  question: "IaaS stands for?",
+  answer: "Infrastructure as a Service",
+  options: [
+    "Internet as a Service",
+    "IOT as a Service",
+    "Infrastructure as a Service",
+    "Infrastructure as a Servicing"
+  ]
+},
+{
+numb: 7,
+question: "In CSS styling can be done by?",
+answer: "All of the above",
+options: [
+  "id",
+  "class",
+  "tag",
+  "All of the above"
+]
+},
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
